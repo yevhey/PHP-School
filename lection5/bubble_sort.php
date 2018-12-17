e87a83a48f31378afe760a6c65349d026346c0fe
Yevhenii Zhdan <yevhenii7009@gmail.com>
2018-12-17 21:50:13 +0200

    hometask after 12/13/2018

---
+++ lection5/bubble_sort.php
@@ -0,0 +1,15 @@
+<?php
+
+$A = [3, 5, 2, 4, 7, 1, 3, 2, 6];
+
+for ($i = 0; $i < count($A); $i++) {
+    for ($j = count($A) - 1; $j >= $i; $j--) {
+        if ($A[$j] < $A[$j - 1]) {
+            $k = $A[$j];
+            $A[$j] = $A[$j - 1];
+            $A[$j - 1] = $k;
+        }
+    }
+}
+
+echo json_encode($A) . PHP_EOL;
